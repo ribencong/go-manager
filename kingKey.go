4a2f3de61ecb7f50febd1a40d81ee240059763aa
ribencong <ribencong@outlook.com>
2019-04-10 10:43:10 +0800

    refactor

--- kingKey.go
+++ kingKey.go
@@ -5,28 +5,21 @@ import (
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/youpipe/go-youPipe/account"
 	"golang.org/x/crypto/ed25519"
-	"io/ioutil"
 	"time"
 )
 
 const (
-	ConfFile          = "finger.json"
 	SysTimeFormat     = "2006-01-02"
 	LicenseTimeFormat = "2006-01-02 15:04:05"
 
 	//debug
-	Address    = "YPDYo3TZsLMgTHF9Vmm9arAWZHAuPTyh8XdF4MzRcqUjuT"
-	CipherText = "ffHuPBsZ7mMZ5m6XDnv66kq2fobLe1TACc4MqKjSY3ELSrvxmTwvmf6tfGsJqXFRN1fKEHZw5dnqdiHw484HiEGkcVXDXNwQhgprQr59NAVoe"
+	//Address    = "YPDYo3TZsLMgTHF9Vmm9arAWZHAuPTyh8XdF4MzRcqUjuT"
+	//CipherText = "ffHuPBsZ7mMZ5m6XDnv66kq2fobLe1TACc4MqKjSY3ELSrvxmTwvmf6tfGsJqXFRN1fKEHZw5dnqdiHw484HiEGkcVXDXNwQhgprQr59NAVoe"
 
 	//release
-	//Address       = "YP5rttHPzRsAe2RmF52sLzbBk4jpoPwJLtABaMv6qn7kVm"
-	//CipherText    = "347FrZuRaDL7dKGeG1fWzZuf2irc3qtXjxpSn762uNxHi8wBjTDongteyLvNDykbnTcXKokvhnvV3kMmnMP1RSYjRUwaGLAGVpkdfkx6CQWKiq"
+	Address    = "YP5rttHPzRsAe2RmF52sLzbBk4jpoPwJLtABaMv6qn7kVm"
+	CipherText = "347FrZuRaDL7dKGeG1fWzZuf2irc3qtXjxpSn762uNxHi8wBjTDongteyLvNDykbnTcXKokvhnvV3kMmnMP1RSYjRUwaGLAGVpkdfkx6CQWKiq"
 )
-
-type SysConf struct {
-	KingKey   string `json:"KingKey"`
-	CipherTxt string `json:"CipherTxt"`
-}
 
 type License struct {
 	Signature string `json:"signature"`
@@ -36,8 +29,6 @@ type License struct {
 }
 
 type ThanosFinger ed25519.PrivateKey
-
-var SysConfig = &SysConf{}
 
 func OpenThanosFinger(password string) ThanosFinger {
 
@@ -54,15 +45,6 @@ func OpenThanosFinger(password string) ThanosFinger {
 
 	if ok := acc.UnlockAcc(password); !ok {
 		panic("You're not Thanos")
-	}
-
-	data, err := ioutil.ReadFile(ConfFile)
-	if err != nil {
-		panic(err)
-	}
-
-	if err = json.Unmarshal(data, SysConfig); err != nil {
-		panic(err)
 	}
 
 	return ThanosFinger(acc.Key.PriKey)
